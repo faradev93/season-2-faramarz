c1b0bad49bbebd2195b1a82add422a275b7a3354
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-09-07 21:59:57 +0330

    test 2

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-let age2 = 30;
-let people = ["faramarz", 31, false, age2];
-console.log(people);
-console.log(typeof people);
-let Arrayiu = [1, 2, "fer", true];
-Array = [56, 22];
-console.log(Arrayiu.length);
-// testttttttttttttttttttttttttttttttttt LooP//
-for (let p = 0; p < 5; p++) {
-    console.log(p);
+// let age2 = 30;
+// let people = ["faramarz", 31, false, age2];
+// console.log(people);
+// console.log(typeof people);
+// let Arrayiu = [1, 2, "fer", true];
+// Array = [56, 22];
+// console.log(Arrayiu.length);
+// // testttttttttttttttttttttttttttttttttt LooP//
+// for (let p = 0; p < 5; p++) {
+//     console.log(p);
+// }
+// for (let fara = 5; fara <= 20; fara++) {
+//     console.log(fara);
+//     console.log(typeof fara);
+// }
+// let Arrayman = ["fara", [people], "30"]
+// console.log(Arrayman);
+// myobject = { name: "faramarz", lastname: "RK", age: 100 };
+// console.log(Object.values(myobject));
+
+typer(["faramarz", 3, 5, 2, 3, "string", true, function () { }]);
+function typer(input) {
+    for (let i = 0; i = input; i++) ;
+        console.log(input);
 }
-for (let fara = 0; fara<= 20 ;fara++){
-    console.log(fara);
-}
-let Arrayman=["fara",[people],"30"]
-console.log(Arrayman);
-myobject={name:"faramarz",lastname:"RK",age:31};
-console.log(myobject);
+
+
+    console.log(typeof typer);
---
+++ scriptjadid2.js
@@ -0,0 +1,7 @@
+let fara2 = (["faramarz", 3, 5, 110, "string", true, function () { }]);
+function test(input) {
+    for (let i = 0; i < input.length; i++) {
+        console.log(input[i])
+    }
+}
+test(fara2);

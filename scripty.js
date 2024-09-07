cc0b66d637a7edd7f46ea154db5c3a620ad5b793
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-09-08 00:00:35 +0330

    test bazam

---
+++ scripty.js
@@ -0,0 +1,16 @@
+// input = [1, 2, 3, "feri", function () { }, true, 540, null, "string"]
+// function spot(input) {
+//     let spot = undefined;
+// }
+// console.log(typeof input);
+// for (let a = 0; a < input.length; a++) {
+//     console.log(input[a]);
+// }
+
+// Object={([1,2,3,"faramarz","RK,"true]),}
+inputy = ["faramarz", "rk", 31, true, null, 5, false]
+function feris() {
+    for (let index = 0; index < feris.length; index++) {
+console.log(feris[index]);
+    }
+}

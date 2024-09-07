b62c3ba31dddc28b7871133112fa1f8e813c565d
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-09-07 21:07:37 +0330

    loop ha

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-let age2 =30;
-let people = ["faramarz",31,false, age2];
+let age2 = 30;
+let people = ["faramarz", 31, false, age2];
 console.log(people);
-console.log( typeof people);
+console.log(typeof people);
+let Arrayiu = [1, 2, "fer", true];
+Array = [56, 22];
+console.log(Arrayiu.length);
+// testttttttttttttttttttttttttttttttttt LooP//
+for (let p = 0; p < 5; p++) {
+    console.log(p);
+}
+for (let fara = 0; fara<= 20 ;fara++){
+    console.log(fara);
+}
+let Arrayman=["fara",[people],"30"]
+console.log(Arrayman);
+myobject={name:"faramarz",lastname:"RK",age:31};
+console.log(myobject);

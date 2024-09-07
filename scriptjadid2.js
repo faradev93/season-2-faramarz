1991fc4da249f20987286c31355593fe9a8d9830
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-09-07 23:53:32 +0330

    test complete

--- scriptjadid2.js
+++ scriptjadid2.js
@@ -1,4 +1,5 @@
 let fara2 = (["faramarz", 3, 5, 110, "string", true, function () { }]);
+console.log(typeof fara2);
 function test(input) {
     for (let i = 0; i < input.length; i++) {
         console.log(input[i])

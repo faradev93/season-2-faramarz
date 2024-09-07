ecbc761c5770d4c55cbee2bf2a0625c5169897b1
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-09-08 00:19:17 +0330

    ..

--- scripty.js
+++ scripty.js
@@ -1,16 +1,17 @@
-// input = [1, 2, 3, "feri", function () { }, true, 540, null, "string"]
-// function spot(input) {
-//     let spot = undefined;
-// }
-// console.log(typeof input);
-// for (let a = 0; a < input.length; a++) {
-//     console.log(input[a]);
-// }
+input = [1, 2, 3, "feri", function () { }, true, 540, null, "string"]
+function spot(input) {
+    let spot = undefined;
+}
+console.log(typeof input);
+for (let a = 0; a < input.length; a++) {
+    console.log(input[a]);
+}
 
 // Object={([1,2,3,"faramarz","RK,"true]),}
-inputy = ["faramarz", "rk", 31, true, null, 5, false]
-function feris() {
-    for (let index = 0; index < feris.length; index++) {
-console.log(feris[index]);
+
+farangiz = [1, "faramarz", "RK", 1372, "1993", 'Married:NO', false, true, function () { }];
+function Giona(farangiz) {
+    for (x = 0; x < farangiz.length; x++){
+        console.log(farangiz[x]);
     }
 }
